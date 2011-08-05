9ec63f2aac50ea9fc414232628cc5d925a5ddc1d
Ben Burry <ben@mindcandy.com>
2011-08-05 18:38:22 +0100

    Change logic to require 2-stage authsub setup Swear at Google and their broken login system

--- handle_incoming_email.py
+++ handle_incoming_email.py
@@ -3,29 +3,38 @@ from google.appengine.ext.webapp.mail_handlers import InboundMailHandler
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.api import memcache
 from google.appengine.api import mail
+from google.appengine.api import users
 from email.utils import parsedate
 from models import Event
 from datetime import datetime
 import time
+import traceback
 import gdata.alt.appengine
 import gdata.calendar
 import gdata.calendar.service
+import gdata.gauth
 import atom
 import logging
 import os
 
 DEBUG = os.getenv('SERVER_SOFTWARE').split('/')[0] == "Development" if os.getenv('SERVER_SOFTWARE') else False
 
-GDATA_TOKEN = "1/k6mv81Oi9C9QfiRtfTl-TYDRADKetfBrE9EE1cSPGhc"
+#GDATA_TOKEN = "1/k6mv81Oi9C9QfiRtfTl-TYDRADKetfBrE9EE1cSPGhc"
 GCAL_ID = "mindcandy.com_kfnhda4e3d0v7dinf08340u2hk@group.calendar.google.com"
 
 class IncomingEventHandler(InboundMailHandler):
     def __init__(self):
         self.calendar_client = gdata.calendar.service.CalendarService()
         gdata.alt.appengine.run_on_appengine(self.calendar_client)
-        self.calendar_client.SetAuthSubToken(GDATA_TOKEN)
+        #self.calendar_client.SetAuthSubToken(GDATA_TOKEN)
             
     def receive(self, mail_message):
+        session_key = gdata.gauth.ae_load(users.get_current_user().user_id())
+        logging.debug("Session key: %s" % session_key.token_string)
+        if not session_key: return self.authenticate(mail_message)
+        
+        self.calendar_client.SetAuthSubToken(session_key.token_string)
+        
         logging.debug("Received a message from: " + mail_message.sender)
         logging.debug("Message received date: " + mail_message.date)        
         # logging.debug("time.strftime('%Y-%m-%dT%H:%M:%S.000Z', time.gmtime()): " + time.strftime('%Y-%m-%dT%H:%M:%S.000Z', time.gmtime()))
@@ -35,35 +44,56 @@ class IncomingEventHandler(InboundMailHandler):
         event_time = time.mktime(parsedate(mail_message.date))
         # logging.debug("time.mktime(email.utils.parsedate('%s')): %s" % (mail_message.date, event_time))
         
-        event = Event(sender=mail_message.sender,
-                         sent=datetime.utcfromtimestamp(event_time),
-                         subject=mail_message.subject,
-                         message=plaintext_bodies[1].decode())
+        try:
+            event = Event(sender=mail_message.sender,
+                             sent=datetime.utcfromtimestamp(event_time),
+                             subject=mail_message.subject,
+                             message=plaintext_bodies[1].decode())
                 
-        event_entry = gdata.calendar.CalendarEventEntry()
-        event_entry.title = atom.Title(text=event.subject)
-        event_entry.content = atom.Content(text=event.message)
-        start_time = time.strftime('%Y-%m-%dT%H:%M:%S.000Z', time.gmtime(event_time))
-        # logging.debug("time.strftime('%%Y-%%m-%%dT%%H:%%M:%%S.000Z', time.gmtime('%s')): %s" % (event_time, start_time))
-        logging.debug("GCal start time: " + start_time)
+            event_entry = gdata.calendar.CalendarEventEntry()
+            event_entry.title = atom.Title(text=event.subject)
+            event_entry.content = atom.Content(text=event.message)
+            start_time = time.strftime('%Y-%m-%dT%H:%M:%S.000Z', time.gmtime(event_time))
+            # logging.debug("time.strftime('%%Y-%%m-%%dT%%H:%%M:%%S.000Z', time.gmtime('%s')): %s" % (event_time, start_time))
+            logging.debug("GCal start time: " + start_time)
        
-        event_entry.when.append(gdata.calendar.When(start_time=start_time))
+            event_entry.when.append(gdata.calendar.When(start_time=start_time))
         
-        cal_event = self.calendar_client.InsertEvent(event_entry, 
-                                'http://www.google.com/calendar/feeds/%s/private/full' % GCAL_ID)
-        alternate_link = cal_event.GetHtmlLink()
-        logging.debug("Created gcal event at " + alternate_link.href)
-        if alternate_link and alternate_link.href:
-            event.event_url = alternate_link.href
+            cal_event = self.calendar_client.InsertEvent(event_entry, 
+                                    'http://www.google.com/calendar/feeds/%s/private/full' % GCAL_ID)
+            alternate_link = cal_event.GetHtmlLink()
+            logging.debug("Created gcal event at " + alternate_link.href)
+            if alternate_link and alternate_link.href:
+                event.event_url = alternate_link.href
             
-        event.put()
-        memcache.delete("homepage")
+            event.put()
+            memcache.delete("homepage")
         
+        except Exception, ex:
+            logging.exception("Unable to store event:\n %s" % traceback.format_exc())
+            request_error = ex[0]
+            if request_error['status'] == 401 or request_error['status'] == 403:
+                self.authenticate(mail_message)
+                return
+            else:
+                mail.send_mail(sender=mail_message.to,
+                                to=mail_message.sender,
+                                subject="Re: %s" % mail_message.subject,
+                                body="ERROR: We were unable to store your event:\n %s" % traceback.format_exc())
+                
+        else:
+            mail.send_mail(sender=mail_message.to,
+                            to=mail_message.sender,
+                            subject="Re: %s" % mail_message.subject,
+                            body="Your event has been stored: %s" % event.event_url)
+
+    def authenticate(self, mail_message):
+        auth_url = gdata.gauth.generate_auth_sub_url('https://mc-towel.appspot.com/auth', 'http://www.google.com/calendar/feeds/', secure=False, session=True, domain='mindcandy.com')
+        logging.debug("Generated auth_url " + str(auth_url))
         mail.send_mail(sender=mail_message.to,
                         to=mail_message.sender,
                         subject="Re: %s" % mail_message.subject,
-                        body="Your event has been stored: %s" % event.event_url)
-
+                        body="ERROR: Please visit this url for authorization:\n %s" % auth_url)
 def main():
     logging.getLogger().setLevel(logging.DEBUG if DEBUG else logging.WARN)
 
--- main.py
+++ main.py
@@ -4,15 +4,19 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
 from google.appengine.api import memcache
+from google.appengine.api import users
 from models import Event
-import gdata
+import gdata.alt.appengine
+import gdata.calendar
+import gdata.calendar.service
+import gdata.gauth
 import os
 import logging
 
 DEBUG = os.getenv('SERVER_SOFTWARE').split('/')[0] == "Development" if os.getenv('SERVER_SOFTWARE') else False
 
 class MainHandler(webapp.RequestHandler):
-    def get(self):
+    def get(self):                        
         page = memcache.get("homepage")
         if page is None:
             events = Event.all().order("-sent").fetch(1000)
@@ -21,11 +25,26 @@ class MainHandler(webapp.RequestHandler):
             memcache.add("homepage", page)
         self.response.out.write(page)
         
+class AuthHandler(webapp.RequestHandler):
+    def __init__(self):
+        self.calendar_client = gdata.calendar.service.CalendarService()
+        gdata.alt.appengine.run_on_appengine(self.calendar_client)
+        
+    def get(self):
+        auth_token = gdata.auth.extract_auth_sub_token_from_url(self.request.uri)
+        if auth_token:
+            session_token = self.calendar_client.upgrade_to_session_token(auth_token)
+            if session_token and users.get_current_user():
+                new_style_token = gdata.gauth.AuthSubToken(session_token.get_token_string(), ['http://calendar.google.com/feeds/'])
+                gdata.gauth.ae_save(new_style_token, users.get_current_user().user_id())
+        self.redirect('/')
+        
 def main():
     logging.getLogger().setLevel(logging.DEBUG if DEBUG else logging.WARN)
 
     application = webapp.WSGIApplication([
                                             (r'/$', MainHandler),
+                                            (r'/auth$', AuthHandler)
                                          ], debug=DEBUG)
     run_wsgi_app(application)
 
